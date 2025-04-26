97ba9f771ebcdfdc52862066be2b73e9ffa2ccde
MahabubArafat <arafatmahabub37@gmail.com>
2025-04-26 22:10:05 +0600

    recharge ui improvements

--- server/routes/tokenRoutes.js
+++ server/routes/tokenRoutes.js
@@ -14,10 +14,10 @@ const { authenticate, authorizeRoles } = require('../middleware/auth');
 // Only admin or provost can charge tokens
 router.post('/charge', authenticate, authorizeRoles('student','admin', 'provost'), chargeTokens);
 router.get('/transactions/student/:studentId', authenticate, getTransactionsByStudent);
+router.get('/transactions', authenticate, authorizeRoles('student','admin', 'provost'), getAllTransactions);
 
 // as students will not be able to modify these things
 router.post('/transactions', authenticate, authorizeRoles('admin', 'provost'), createTransaction);
-router.get('/transactions', authenticate, authorizeRoles('admin', 'provost'), getAllTransactions);
 router.put('/transactions/:id', authenticate, authorizeRoles('admin', 'provost'), updateTransaction);
 router.delete('/transactions/:id', authenticate, authorizeRoles('admin', 'provost'), deleteTransaction);
 
